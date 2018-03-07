d7da341531f0960adfa2492d6354e131e375f856
Tim Geraghty <timg2106@gmail.com>
2018-03-06 21:10:20 -0800

    changes to

--- ChatterBot.py
+++ ChatterBot.py
@@ -4,10 +4,11 @@ import json
 import time
 
 # Twitter API Keys
-consumer_key = "Ed4RNulN1lp7AbOooHa9STCoU"
-consumer_secret = "P7cUJlmJZq0VaCY0Jg7COliwQqzK0qYEyUF9Y0idx4ujb3ZlW5"
-access_token = "839621358724198402-dzdOsx2WWHrSuBwyNUiqSEnTivHozAZ"
-access_token_secret = "dCZ80uNRbFDjxdU2EckmNiSckdoATach6Q8zb7YYYE5ER"
+consumer_key = 'sabuUUyKD7AGfaTzurpRGmdYP'
+consumer_secret = 'DJTqZcwS74QjkpY9uJ08X6Ahn2eHLCyhuaEwrM0AvDaK5MDXtB'
+access_token = '801373896649752576-hW9RPhvqpZ8BCOS0kpZfCwLIulOP7wu'
+access_token_secret = 'c5DLoSUDVtAQy2fgyx0WN7pMklIr9UqTENtPXxDWq9FHV'
+openweather_api_key = "25bc90a1196e6f153eece0bc0b0fc9eb"
 
 # Setup Tweepy API Authentication
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
